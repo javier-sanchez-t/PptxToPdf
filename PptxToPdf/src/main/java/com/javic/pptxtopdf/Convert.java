98ede8d5ede875a85d6b77b162ab9501c09d4fb8
DESKTOP-BB4RO6F\acer <sanchez.javier@mbn.com.mx>
2018-04-14 17:07:32 -0500

    JST - getting notes from each slide

--- PptxToPdf/src/main/java/com/javic/pptxtopdf/Convert.java
+++ PptxToPdf/src/main/java/com/javic/pptxtopdf/Convert.java
@@ -10,8 +10,6 @@ import com.lowagie.text.DocumentException;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
-import org.apache.poi.hslf.model.Slide;
-import org.apache.poi.hslf.usermodel.SlideShow;
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 
@@ -21,7 +19,6 @@ import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import org.apache.poi.xslf.usermodel.XSLFNotes;
-import org.apache.poi.xslf.usermodel.XSLFTextParagraph;
 import org.apache.poi.xslf.usermodel.XSLFTextShape;
 
 /**
@@ -29,23 +26,24 @@ import org.apache.poi.xslf.usermodel.XSLFTextShape;
  * @author acer
  */
 public class Convert {
+
     // http://javapro.org/castellano/2017/07/25/convertir-archivo-powerpoint-pdf-java/
     public static void main(String[] args) {
-        try {
-            Convert.convertPPTToPDF("C:\\Users\\acer\\Desktop\\test.pptx", "C:\\Users\\acer\\Desktop\\p1.pdf", ".pptx");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Convert.convertPPTToPDF("C:\\Users\\acer\\Desktop\\test.pptx", "C:\\Users\\acer\\Desktop\\p1.pdf", ".pptx");
     }
-    
-    public static void convertPPTToPDF(String sourcepath, String destinationPath, String fileType) {
+
+    public static void convertPPTToPDF(String sourcePathFile, String destinationPath, String fileType) {
         double zoom = 2;
         AffineTransform at = new AffineTransform();
         at.setToScale(zoom, zoom);
+
+        //Final output file
         Document pdfDocument = new Document();
-        byte[] barr = new byte[0];
-        try (FileInputStream inputStream = new FileInputStream(sourcepath);
-                ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+
+        try {
+            FileInputStream inputStream = new FileInputStream(sourcePathFile);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
             PdfWriter pdfWriter = PdfWriter.getInstance(pdfDocument, baos);
             PdfPTable table = new PdfPTable(1);
             pdfWriter.open();
@@ -53,75 +51,65 @@ public class Convert {
             Dimension pgsize = null;
             com.lowagie.text.Image slideImage = null;
             BufferedImage img = null;
-            if (fileType.equalsIgnoreCase(".ppt")) {
-                SlideShow ppt = new SlideShow(inputStream);
-                pgsize = ppt.getPageSize();
-                Slide slide[] = ppt.getSlides();
-                pdfDocument.setPageSize(new com.lowagie.text.Rectangle((float) pgsize.getWidth(), (float) pgsize.getHeight()));
-                pdfWriter.open();
-                pdfDocument.open();
-                for (int i = 0; i < slide.length; i++) {
-                    img = new BufferedImage((int) Math.ceil(pgsize.width * zoom), (int) Math.ceil(pgsize.height * zoom), BufferedImage.TYPE_INT_RGB);
-                    Graphics2D graphics = img.createGraphics();
-                    graphics.setTransform(at);
-                    
-                    graphics.setPaint(Color.white);
-                    graphics.fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height));
-                    slide[i].draw(graphics);
-                    graphics.getPaint();
-                    slideImage = com.lowagie.text.Image.getInstance(img, null);
-                    table.addCell(new PdfPCell(slideImage, true));
-                }
-            }
+
             if (fileType.equalsIgnoreCase(".pptx")) {
                 XMLSlideShow ppt = new XMLSlideShow(inputStream);
                 pgsize = ppt.getPageSize();
-                //nuevo
-                XSLFSlide slide1 = ppt.getSlides().get(0);
-                XSLFNotes note = ppt.getNotesSlide(slide1);
-                System.out.println("TEXTO==============>");
-                /*for (XSLFTextShape shape : note.getPlaceholders()) {
-                    System.out.println("texto: "+shape.getText());
-                }*/
-                //String nota=note.getPlaceholder(1).getText();
-                
-                java.util.List<org.apache.poi.xslf.usermodel.XSLFSlide> slide = ppt.getSlides();
+
                 pdfDocument.setPageSize(new com.lowagie.text.Rectangle((float) pgsize.getWidth(), (float) pgsize.getHeight()));
                 pdfWriter.open();
                 pdfDocument.open();
-                for (XSLFSlide slid : slide) {
+
+                for (int i = 0; i < ppt.getSlides().size(); i++) {
+                    XSLFSlide slid = ppt.getSlides().get(i);
+
+                    //Gets note 
+                    String note = getNoteFromSlide(ppt, slid);
+
                     img = new BufferedImage((int) Math.ceil(pgsize.width * zoom), (int) Math.ceil(pgsize.height * zoom), BufferedImage.TYPE_INT_RGB);
                     Graphics2D graphics = img.createGraphics();
                     graphics.setTransform(at);
-                    
+
                     graphics.setPaint(Color.white);
                     graphics.fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height));
                     slid.draw(graphics);
                     graphics.getPaint();
                     slideImage = com.lowagie.text.Image.getInstance(img, null);
-                    table.addCell(new PdfPCell(slideImage, true));
-                    //table.addCell(nota);
+
+                    //Adds slide image
+                    //table.addCell(new PdfPCell(slideImage, true));
+                    table.addCell(slideImage);
+                    
+                    //Adds note
+                    table.addCell(note);
                 }
             }
+
             pdfDocument.add(table);
             pdfDocument.close();
-            barr = baos.toByteArray();
+            byte[] barr = baos.toByteArray();
             pdfWriter.close();
             System.out.println("Powerpoint file converted to PDF successfully");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        }
-        
-        try (FileOutputStream outputStream = new FileOutputStream(new File(destinationPath))) {
+
+            FileOutputStream outputStream = new FileOutputStream(new File(destinationPath));
             outputStream.write(barr);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException | DocumentException e) {
         }
+
+    }
+
+    /**
+     * Get main note from specific slide
+     *
+     * @param ppt
+     * @param slide
+     * @return
+     */
+    public static String getNoteFromSlide(XMLSlideShow ppt, XSLFSlide slide) {
+        XSLFNotes notes = ppt.getNotesSlide(slide);
+        XSLFTextShape shape = notes.getPlaceholder(1);
+        String note = shape.getText();
+        return note;
     }
 }
